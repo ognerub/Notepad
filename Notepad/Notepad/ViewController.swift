802be257c5f6062da64e1389608564c56ee3fd3c
ognerub <a.ulianovskiy@yandex.ru>
2024-01-27 15:45:12 +0300

    navigation bar and plus button added

--- Notepad/Notepad/ViewController.swift
+++ Notepad/Notepad/ViewController.swift
@@ -9,28 +9,63 @@ import UIKit
 
 final class ViewController: UIViewController {
     
-    private let array: [Int] = Array(0...30)
+    private lazy var navigationBar: UINavigationBar = {
+        let bar = UINavigationBar()
+        bar.layer.backgroundColor = UIColor.clear.cgColor
+        bar.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 100)
+        return bar
+    }()
+    
+    private lazy var plusButton: UIButton = {
+        let button = UIButton.systemButton(
+            with: UIImage(systemName: "plus") ?? UIImage(),
+            target: self,
+            action: #selector(plusButtonTapped)
+        )
+        button.tintColor = UIColor.blue
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let array: [Int] = Array(0...5)
     
     private let tableView: UITableView = {
         let table = UITableView()
-        table.backgroundColor = .green
+        table.backgroundColor = .white
         table.translatesAutoresizingMaskIntoConstraints = false
         return table
     }()
 
+    
+    // MARK: - View controller lifecycle methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .white
         configureConstraints()
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register(TableViewCustomCell.self, forCellReuseIdentifier: TableViewCustomCell.cellReuseIdentifier)
     }
     
+    @objc
+    private func plusButtonTapped() {
+        print("plusButtonTapped")
+    }
+    
     private func configureConstraints() {
+        view.addSubview(navigationBar)
+        navigationBar.addSubview(plusButton)
+        NSLayoutConstraint.activate([
+            plusButton.bottomAnchor.constraint(equalTo: navigationBar.bottomAnchor),
+            plusButton.trailingAnchor.constraint(equalTo: navigationBar.trailingAnchor, constant: -5),
+            plusButton.heightAnchor.constraint(equalToConstant: 42),
+            plusButton.widthAnchor.constraint(equalToConstant: 42)
+            
+        ])
         view.addSubview(tableView)
         NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.topAnchor.constraint(equalTo: navigationBar.bottomAnchor),
             tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
